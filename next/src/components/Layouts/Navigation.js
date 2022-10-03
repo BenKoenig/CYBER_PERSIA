c0be321e92f294c1ff03cf94bb6db6820bf151a7
BenKoenig <bkoenig.as@gmail.com>
2022-10-03 17:55:03 +0200

    update nav

--- next/src/components/Layouts/Navigation.js
+++ next/src/components/Layouts/Navigation.js
@@ -18,7 +18,7 @@ const Navigation = () => {
 
         <nav className=" border-b border-gray-100 bg-white relative">
             {/* Primary Navigation Menu */}
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex">
                         {/* Logo */}
