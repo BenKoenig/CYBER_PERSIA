ff1e13ec67da35ec5911ecaea823b9bd50693bdb
mimitamara <gavrielovd@gmail.com>
2022-10-29 14:36:37 +0200

    example

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -5,13 +5,19 @@ module.exports = {
     darkMode: 'media',
     theme: {
         extend: {
-/*            fontFamily: {
+            /*            fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },*/
             colors: {
                 'light-100': '#C0E8FF',
-                'light-50': '#DCF2FF'
-            }
+                'light-50': '#DCF2FF',
+                'light-blue': '#2932F5',
+                'dark-purple': '#260273',
+                'dark-blue': '#013075',
+                'sp-black': '#011526',
+                'dark-yellow': '#F5F001',
+                'light-beige': '#F6F6EB',
+            },
         },
     },
     variants: {
